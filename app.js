f59be7a82c6d6c1b8d8967a7179d1be85cc28db7
OJO SEUN <ojoseun399@gmail.com>
2023-01-22 16:22:35 +0100

    server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).json([
+    { name: "OJO", age: 20 },
+    { name: "Seun", age: 30 },
+    { name: "Seye", age: 10 },
+    { name: "Yori", age: 35 },
+    { name: "Doyong", age: 12 },
+    { name: "Fury", age: 40 },
+    { name: "Rypoyu", age: 45 },
+  ]);
+});
+
+app.listen(4000, () => console.log("Container Start Running"));
